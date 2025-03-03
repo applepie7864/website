2ecd16c1c03d1d64eb2cae39068103044f36b188
applepie7864 <anniewenqingguo@gmail.com>
2025-03-03 15:36:07 -0500

    saving progress

---
+++ src/components/ExperienceCard.tsx
@@ -0,0 +1,11 @@
+import { Experience } from "../types"
+
+const ExperienceCard = ({ company, title, date, location, website, logo }: Experience) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ExperienceCard
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,8 @@
-export {}
+export type Experience = {
+    company: String; 
+    title: String;
+    date: String;
+    location: String;
+    website: String;
+    logo: String;
+}
