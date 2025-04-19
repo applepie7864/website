5b68c22069cac6ad503a844907690af04c39a127
applepie7864 <anniewenqingguo@gmail.com>
2025-04-19 11:15:47 -0400

    save

--- src/components/common/side-bar.tsx
+++ src/components/common/side-bar.tsx
@@ -4,7 +4,7 @@ import ThemeToggle from "./theme-toggle";
 
 export default function SideBar() {
   return (
-    <div className="fixed top-0 left-0 w-[35rem] h-full bg-gray-50 dark:bg-gray-950 px-20 py-16 flex flex-col items-left justify-between">
+    <div className="fixed top-0 left-0 w-[35rem] h-full bg-gray-50 dark:bg-gray-950 px-20 py-16 flex flex-col items-left justify-between fade-in">
       <div className="flex flex-col">
         <div className="text-6xl bold text-gray-800 dark:text-gray-200">{data.general.name}</div>
         <div className="text-3xl medium text-gray-500 mt-1 mb-5">{data.general.title}</div>
