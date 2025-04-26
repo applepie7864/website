5cecc0eebc6de18101c5d47342f7bbeb8000145e
applepie7864 <anniewenqingguo@gmail.com>
2025-04-26 14:30:49 -0400

    headings

--- src/components/sections/experience/main.tsx
+++ src/components/sections/experience/main.tsx
@@ -5,6 +5,7 @@ import ExperienceCard from './experience-card';
 const ExperienceSection = () => {
   return (
     <section id="experience" className='flex flex-col gap-2'>
+      <div className="block desktop:hidden text-2xl medium">Experience</div>
       {data.experiences.map((experience: Experience) => (
         <ExperienceCard {...experience} />
       ))}
--- src/components/sections/projects/main.tsx
+++ src/components/sections/projects/main.tsx
@@ -5,6 +5,7 @@ import ProjectCard from './project-card';
 const ProjectSection = () => {
   return (
     <section id="projects">
+      <div className="block desktop:hidden text-2xl medium mb-2">Projects</div>
       <div className="w-full grid grid-cols-1 desktop:grid-cols-2 gap-4">
         {data.projects.map((project: Project) => (
           <ProjectCard {...project} />
